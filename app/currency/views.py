ccddcab8e66636c6761cc0012eeadb3cc92353f0
zimarinvitalii <zimarin96@gmail.com>
2021-08-19 16:37:46 +0300

    initial commit

---
+++ app/currency/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from currency.utils import generate_password as gen_pass
+# Create your views here.
+
+def hello_world(request):
+    return HttpResponse('Hello World')
+
+def generate_password(request):
+    password_len = int(request.GET.get('password-len'))
+    password = gen_pass(password_len)
+    return HttpResponse(password)
