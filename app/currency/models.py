558e6aabea59db0444d7689f6ed306192242908e
zimarinvitalii <76126348+zimarinvitalii@users.noreply.github.com>
2021-08-28 01:16:33 +0300

    Revert "Create ContactUs model in currency app"

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-
-class Rate(models.Model):
-    sale = models.DecimalField(max_digits=5, decimal_places=2)
-    buy = models.DecimalField(max_digits=5, decimal_places=2)
-    created = models.DateTimeField(auto_now_add=True)
-    source = models.CharField(max_length=32)  # privat, monobank
-    type = models.CharField(max_length=3)  # USD, EUR
-    display_name = models.CharField(max_length=12)
-
-
-class ContactUs(models.Model):
-    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    email_form = models.EmailField(max_length=50)
-    subject = models.CharField(max_length=50, default="", editable=False)
-    message = models.CharField(max_length=5000, default="", editable=True)
+# Create your models here.
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,15 +1,12 @@
-from currency.views import contact_us, generate_password, hello_world
-
 from django.contrib import admin
-
 from django.urls import path
 
+from currency.views import hello_world, generate_password
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-
+    #currency
     path('hello-world/', hello_world),
     path('gen-pass/', generate_password),
-    path('cont-us/', contact_us),
 ]
